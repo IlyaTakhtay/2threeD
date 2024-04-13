1ba7f6a139ca070eac86e66541e42c372f938598
Ilya <i.tahtaj@gmail.com>
2024-04-13 13:00:44 +0300

    added one more plane

--- MVC/main.js
+++ MVC/main.js
@@ -14,20 +14,29 @@ const planeModelXY = new PlaneModel;
 const planeControllerXY = new PlaneController(planeModelXY);
 
 const planeViewXY = new PlaneView('containerXY','XYCanvas','XYCanvasCoordinates', 'XYlegend',planeControllerXY);
+
+const planeModelYZ = new PlaneModel;
+
+const planeControllerYZ = new PlaneController(planeModelYZ);
+
+const planeViewYZ = new PlaneView('containerYZ','YZCanvas','YZCanvasCoordinates', 'YZlegend',planeControllerYZ);
 //   const legend = new Legend('XYCanvas', legendData);
 //   legend.render();
 
 window.toggleAddPointMode = function() {
     planeViewXZ.toggleAddPointMode();
     planeViewXY.toggleAddPointMode();
+    planeViewYZ.toggleAddPointMode();
   };
   
 window.toggleAddLineMode = function() {
     planeViewXZ.toggleAddLineMode();
     planeViewXY.toggleAddLineMode();
+    planeViewYZ.toggleAddLineMode();
 };
   
 window.deleteSelected = function() {
     planeViewXZ.deleteSelected();
     planeViewXY.deleteSelected();
+    planeViewYZ.deleteSelected();
 };
