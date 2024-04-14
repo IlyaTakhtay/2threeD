0d03143121721f5779170ed5422f1109a726f411
Ilya <i.tahtaj@gmail.com>
2024-04-15 02:02:25 +0300

    start to reconstruct 3d

--- MVC/main.js
+++ MVC/main.js
@@ -3,29 +3,31 @@ import { PlaneModel } from './planes/planeModel.js';
 import { PlaneView } from './planes/planeView.js';
 import { Legend } from './planes/planeView.js'
 
+import { Plane3D } from './planes/reconstruction.js';
+
 const planeModelXZ = new PlaneModel;
-
 const planeControllerXZ = new PlaneController(planeModelXZ);
-
 const planeViewXZ = new PlaneView('containerXZ','XZCanvas','XZCanvasCoordinates', 'XZegend',planeControllerXZ);
 
 const planeModelXY = new PlaneModel;
-
 const planeControllerXY = new PlaneController(planeModelXY);
-
 const planeViewXY = new PlaneView('containerXY','XYCanvas','XYCanvasCoordinates', 'XYlegend',planeControllerXY);
 
 const planeModelYZ = new PlaneModel;
-
 const planeControllerYZ = new PlaneController(planeModelYZ);
-
 const planeViewYZ = new PlaneView('containerYZ','YZCanvas','YZCanvasCoordinates', 'YZlegend',planeControllerYZ);
 
 planeViewYZ.configure('leftLower')
 planeViewXY.configure('rightUpper')
 planeViewXZ.configure('rightLower')
+
+const plane3D = new Plane3D()
 //   const legend = new Legend('XYCanvas', legendData);
 //   legend.render();
+
+window.reconstruct = function() {
+    plane3D.mainProcess({yzObjects:planeModelYZ.objects, xzObjects: planeModelXZ.objects, xyObjects: planeModelXY.objects});
+}   
 
 window.toggleAddPointMode = function() {
     planeViewXZ.toggleAddPointMode();
--- MVC/planes/reconstruction.js
+++ MVC/planes/reconstruction.js
@@ -1,15 +1,70 @@
+import { Point } from "./planeModel.js";
+import { Line } from "./planeModel.js";
 class Point3D {
-  constructor(x, y, z) {
-    this.x = x;
-    this.y = y;
-    this.z = z;
+  #x;
+  #y;
+  #z;
+  #name;
+  constructor(x=null, y=null, z=null) {
+    this.#x = x;
+    this.#y = y;
+    this.#z = z;
+  }
+
+  get pointX() { return this.#x }
+  get pointY() { return this.#y }
+  get pointZ() { return this.#z }
+
+  get name() { return this.#name }
+
+  set pointX(x) {this.#x = x}
+  set pointY(y) {this.#y = y}
+  set pointZ(z) {this.#y = z}
+
+  set name(name) { this.#name = name }
+
+  equals(other) {
+    return this.#x === other.pointX && this.#y === other.pointY && this.#z === other.pointZ;
   }
 }
 
-class Edge {
-  constructor(v1, v2) {
-    this.v1 = v1;
-    this.v2 = v2;
+class Line3D {
+  #point1;
+  #point2;
+  #name;
+  constructor({point1 = null, point2 = null, name = null}) {
+      this.#point1 = point1;
+      this.#point2 = point2;
+      this.#name = name;
+  }
+  get linePointX1() { return this.#point1.pointX }
+  get linePointY1() { return this.#point1.pointY }
+  get linePointZ1() { return this.#point1.pointZ }
+
+  get linePointX2() { return this.#point2.pointX }
+  get linePointY2() { return this.#point2.pointY }
+  get linePointZ2() { return this.#point2.pointZ }
+
+  set linePointX1(x1) { this.#point1.pointX = x1 }
+  set linePointY1(y1) { this.#point1.pointY = y1 }
+  set linePointZ1(z1) { this.#point1.pointZ = z1 }
+
+  set linePointX2(x2) { this.#point2.pointX = x2 }
+  set linePointY2(y2) { this.#point2.pointY = y2 }
+  set linePointZ2(z2) { this.#point2.pointZ = z2 }
+
+  get firstPoint() { return this.#point1 }
+  get secondPoint() { return this.#point2 }
+
+  get name() { return this.#name };
+
+  set name(name) { this.#name = name }
+  
+  equals(other) {
+    return (
+      (this.firstPoint.equals(other.firstPoint) && this.secondPoint.equals(other.secondPoint)) ||
+      (this.secondPoint.equals(other.endPoint) && this.secondPoint.equals(other.secondPoint))
+    );
   }
 }
 
@@ -19,94 +74,140 @@ class Face {
   }
 }
 
-class PseudowireframeReconstruction {
+export class Plane3D {
+  #vertices;
+  #edges;
+  #faces;
   constructor() {
-    this.vertices = [];
-    this.edges = [];
-    this.faces = [];
+    this.#vertices = [];
+    this.#edges = [];
+    this.#faces = [];
   }
 
-  reconstructModel(planeModelXZ, planeModelYZ, planeModelXY) {
-    // Создание графа смежности
-    const vertexMap = new Map();
-    this.createVertexEdgeGraph(planeModelXZ, vertexMap);
-    this.createVertexEdgeGraph(planeModelYZ, vertexMap);
-    this.createVertexEdgeGraph(planeModelXY, vertexMap);
-
-    // Определение координат вершин
-    this.determineVertexCoordinates(vertexMap);
-
-    // Построение ребер и граней
-    this.buildEdgesAndFaces();
-
-    // Устранение неоднозначностей (если необходимо)
-    this.resolveAmbiguities();
+  pointsExtractor(objects){
+    objects.forEach(element => {
+      if (element instanceof Point) return element
+    });
   }
 
-  visualizeModel() {
-    // Визуализация псевдокаркасной модели
-    // Используйте библиотеку или фреймворк для 3D графики
-    // Например, Three.js
-    // ...
+  linesExtractor(objects){
+    objects.forEach(element => {
+      if (element instanceof Line) return element
+    });
   }
 
-  createVertexEdgeGraph(objects, vertexMap) {
-    for (let i = 0; i < objects.length; i++) {
-      const point = objects[i];
+  inputDataConverter({objects, planeAxes}){
+      console.log(objects)
+      const points = objects.filter(obj => obj instanceof Point);
+      const lines = objects.filter(obj => obj instanceof Line);
+      console.log(points)
+      console.log(lines)
+      let convertedPoints;
+      switch (planeAxes){
+        case 'XY':
+          convertedPoints = points.map(point => new Point3D(point.pointX, point.pointY, null));
+            break;
+        case 'XZ':
+          convertedPoints = points.map(point => new Point3D(point.pointX, null, point.pointY));
+            break;
+        case 'YZ':
+          convertedPoints = points.map(point => new Point3D(null, point.pointX, point.pointY));
+            break;
+    }
       
-      // Проверяем, есть ли уже такая вершина в графе
-      if (!vertexMap.has(point)) {
-        // Если вершины нет, добавляем ее в граф
-        const vertexId = this.vertices.length;
-        this.vertices.push(point);
-        vertexMap.set(point, vertexId);
+      
+      const convertedLines = lines.map(line => {
+        const point1Index = points.indexOf(line.firstPoint);
+        const point2Index = points.indexOf(line.secondPoint);
+
+        return new Line3D({point1:convertedPoints[point1Index], point2:convertedPoints[point2Index]});
+      });
+      
+      return objects.map(obj => {
+        if (obj instanceof Point) {
+          return convertedPoints[points.indexOf(obj)];
+        } else if (obj instanceof Line) {
+          return convertedLines[lines.indexOf(obj)];
+        }
+      });
+  }
+
+  find3DPoints ({frontView, topView, sideView}){
+      const result = [];
+    
+      for (const frontElement of frontView) {
+        if (frontElement instanceof Point3D) {
+          const x1 = frontElement.pointX;
+          const z1 = frontElement.pointZ;
+    
+          for (const topElement of topView) {
+            if (topElement instanceof Point3D && x1 === topElement.pointX) {
+              const y1 = topElement.pointY;
+    
+              for (const sideElement of sideView) {
+                if (sideElement instanceof Point3D && y1 === sideElement.pointY && z1 === sideElement.pointZ) {
+                  const point3D = new Point3D(x1, y1, z1);
+    
+                  // Проверяем, есть ли уже такая точка в результирующем массиве
+                  if (!result.some(p => p.equals(point3D))) {
+                    result.push(point3D);
+                  }
+                }
+              }
+            }
+          }
+        }
       }
-      
-      // Находим индексы вершин для текущей точки и следующей точки (если есть)
-      const currentVertexId = vertexMap.get(point);
-      const nextPoint = objects[(i + 1) % objects.length];
-      const nextVertexId = vertexMap.get(nextPoint);
-      
-      // Проверяем, есть ли уже ребро между текущей и следующей вершинами
-      const edgeExists = this.edges.some(edge =>
-        (edge.v1 === currentVertexId && edge.v2 === nextVertexId) ||
-        (edge.v1 === nextVertexId && edge.v2 === currentVertexId)
-      );
-      
-      // Если ребра нет, добавляем его в граф
-      if (!edgeExists) {
-        this.edges.push(new Edge(currentVertexId, nextVertexId));
+      return result;
+  }
+
+  find3DEdges(points3D, frontEdges, topEdges, sideEdges) {
+    const edges3D = [];
+  
+    for (let i = 0; i < points3D.length; i++) {
+      for (let j = i + 1; j < points3D.length; j++) {
+        const startPoint = points3D[i];
+        const endPoint = points3D[j];
+  
+        const frontEdge = new Edge(
+          new Point3D(startPoint.pointX, null, startPoint.pointZ),
+          new Point3D(endPoint.pointX, null, endPoint.pointZ)
+        );
+        const topEdge = new Edge(
+          new Point3D(startPoint.pointX, startPoint.pointY, null),
+          new Point3D(endPoint.pointX, endPoint.pointY, null)
+        );
+        const sideEdge = new Edge(
+          new Point3D(null, startPoint.pointY, startPoint.pointZ),
+          new Point3D(null, endPoint.pointY, endPoint.pointZ)
+        );
+  
+        if (
+          (frontEdges.some(e => e.equals(frontEdge)) || startPoint.pointX === endPoint.pointX && startPoint.pointZ === endPoint.pointZ) &&
+          (topEdges.some(e => e.equals(topEdge)) || startPoint.pointX === endPoint.pointX && startPoint.pointY === endPoint.pointY) &&
+          (sideEdges.some(e => e.equals(sideEdge)) || startPoint.pointY === endPoint.pointY && startPoint.pointZ === endPoint.pointZ)
+        ) {
+          const edge3D = new Edge(startPoint, endPoint);
+          edges3D.push(edge3D);
+        }
       }
     }
+  
+    return edges3D;
+  }
+  
+
+  mainProcess ({yzObjects, xzObjects, xyObjects}) {
+    const sideView = ({
+      points:this.pointsExtractor(this.inputDataConverter({objects:yzObjects, planeAxes:'YZ'})),
+      lines:this.linesExtractor(this.inputDataConverter({objects:yzObjects, planeAxes:'YZ'}))
+    });
+    const frontView = this.inputDataConverter({objects:xzObjects, planeAxes:'XZ'})
+    const topView = this.inputDataConverter({objects:xyObjects, planeAxes:'XY'})
+    sideView = 
+    console.log("Planes", sideView,frontView,topView)
+    console.log("3Dpoints", this.find3DPoints({sideView:sideView,frontView:frontView,topView:topView}))
   }
 
-  determineVertexCoordinates(vertexMap) {
-    // Определение координат вершин на основе информации из проекций
-    // ...
-  }
 
-  buildEdgesAndFaces() {
-    // Построение ребер и граней на основе графа смежности и координат вершин
-    // ...
-  }
-
-  resolveAmbiguities() {
-    // Устранение неоднозначностей при восстановлении модели
-    // ...
-  }
 }
-
-// Пример использования
-const planeModelXZ = [
-  /* ... */
-]; // Координаты проекции на плоскость XZ
-const planeModelYZ = [
-  /* ... */
-]; // Координаты проекции на плоскость YZ
-const planeModelXY = [
-  /* ... */
-]; // Координаты проекции на плоскость XY
-
-const reconstruction = new PseudowireframeReconstruction();
-reconstruction.reconstructModel(planeModelXZ, planeModelYZ, planeModelXY);
-reconstruction.visualizeModel();
