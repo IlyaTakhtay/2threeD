c11602464643cdee48614bd0bfec71755fb8b790
Ilya <i.tahtaj@gmail.com>
2024-05-29 02:33:48 +0300

    clear foreign files

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  root: 'MVC', // указываем корневую папку проекта
-  build: {
-    outDir: '../dist' // указываем, что собранные файлы должны быть в папке на уровень выше
-  }
-})
