f16e3d9226c2576de7292194c0c72d5211624a56
Ilya <i.tahtaj@gmail.com>
2024-06-16 03:32:21 +0300

    tried to sw

--- desktop_tauri/vite.config.js
+++ desktop_tauri/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   clearScreen: false,
   // Tauri expects a fixed port, fail if that port is not available
   server: {
-    strictPort: true,
+    port: 5173,  // Убедитесь, что порт указан правильно
   },
   // to access the Tauri environment variables set by the CLI with information about the current target
   envPrefix: ['VITE_', 'TAURI_PLATFORM', 'TAURI_ARCH', 'TAURI_FAMILY', 'TAURI_PLATFORM_VERSION', 'TAURI_PLATFORM_TYPE', 'TAURI_DEBUG'],
