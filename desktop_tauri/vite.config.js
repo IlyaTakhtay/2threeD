b727eb6e2e40bb90fc4ad6da59847be82f8ee43d
Ilya <i.tahtaj@gmail.com>
2024-06-16 03:44:23 +0300

    made sw by plugin

--- desktop_tauri/vite.config.js
+++ desktop_tauri/vite.config.js
@@ -1,4 +1,6 @@
 import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'; // Импортируем плагин Vue
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   // prevent vite from obscuring rust errors
@@ -19,4 +21,86 @@ export default defineConfig({
     sourcemap: !!process.env.TAURI_DEBUG,
     outDir: '../dist' // путь для выходных файлов сборки
   },
-})
+  plugins: [
+    VitePWA({
+      manifest: {
+        "name": "2ThreeDimention",
+        "short_name": "2threeD",
+        "description": "Application to make three projections of object and reconstruct them to threeD frame",
+        "theme_color": "#48829d",
+        "background_color": "#5b19d7",
+        "display": "standalone",
+        "orientation": "any",
+        "scope": "/",
+        "start_url": "/",
+        "icons": [
+            {
+                "src": "icons/icon_192.png",
+                "sizes": "192x192",
+                "type": "image/png",
+                "purpose": "any"
+            },
+            {
+                "src": "icons/icon_384.png",
+                "sizes": "384x384",
+                "type": "image/png",
+                "purpose": "any"
+            },
+            {
+                "src": "icons/maskable_192.png",
+                "sizes": "192x192",
+                "type": "image/png",
+                "purpose": "maskable"
+            },
+            {
+                "src": "icons/maskable_384.png",
+                "sizes": "384x384",
+                "type": "image/png",
+                "purpose": "maskable"
+            }
+        ]
+    },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'google-fonts-stylesheets',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365 // 1 год
+              }
+            }
+          },
+          {
+            urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'google-fonts-webfonts',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365 // 1 год
+              }
+            }
+          },
+          {
+            urlPattern: /^https:\/\/cdn\.jsdelivr\.net\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'jsdelivr-cdn',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365 // 1 год
+              }
+            }
+          }
+        ]
+      },
+      devOptions: {
+        enabled: true,
+        type: 'module',
+      }
+    })
+  ]
+});
